bb536d23cd229cf743f8253f641b2e4d18cab35e
tang2988 <tang2988@qq.com>
2016-04-07 17:32:47 +0800

    sql

--- mblog-web/sql/db_init_now.sql
+++ mblog-web/sql/db_init_now.sql
@@ -68,7 +68,7 @@ CREATE TABLE `mto_config` (
 INSERT INTO `mto_config` VALUES ('1', 'site_name', '0', 'Mtons');
 INSERT INTO `mto_config` VALUES ('2', 'site_welcomes', '0', 'Mtons, 轻松分享你的兴趣');
 INSERT INTO `mto_config` VALUES ('3', 'site_domain', '0', 'http://mtons.com');
-INSERT INTO `mto_config` VALUES ('4', 'site_keywords', '0', 'mtons,博客,社区,摄影,旅游,艺术,娱乐');
+INSERT INTO `mto_config` VALUES ('4', 'site_keywords', '0', '51xinbei,博客,社区,摄影,旅游,艺术,娱乐');
 INSERT INTO `mto_config` VALUES ('5', 'site_description', '0', 'Mtons, 轻松分享你的兴趣. 便捷的文字、图片发布,扁平化的响应式设计,美观、大气,是您记录生活的最佳选择');
 INSERT INTO `mto_config` VALUES ('6', 'site_editor', '1', 'ueditor');
 
